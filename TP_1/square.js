fa984e9a73b2a96c8bc555a1b03b59547db29400
Nicolas Ayala <nayala@enssat.fr>
2019-01-30 11:22:09 +0100

    + tp2.html

---
+++ TP_1/square.js
@@ -0,0 +1,19 @@
+function Square(ctx, width, height, color, x, y, dx, dy) {
+	this.context = context
+	this.width = width;
+	this.height = height;
+	this.dx = dx;
+	this.dy = dy;
+	this.x = x;
+	this.y = y;
+
+	this.update = function() {
+		this.context.fillStyle = color;
+		this.context.fillRect(this.x, this.y, this.width, this.height)
+
+		this.x += dx;
+		this.y += dy;
+	}
+
+
+}
